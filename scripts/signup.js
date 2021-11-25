005149c3a8552595a1cd663318d4517bef974462
RamonRuizPe <rualejandro.ra@gmail.com>
2021-11-24 23:42:49 -0600

    Firestore user storage already working

--- scripts/signup.js
+++ scripts/signup.js
@@ -1,33 +1,62 @@
-import { getAuth, createUserWithEmailAndPassword } from 'https://www.gstatic.com/firebasejs/9.1.2/firebase-auth.js';
+import app from "./index.js";
+import {getFirestore, collection, doc, setDoc} from "https://www.gstatic.com/firebasejs/9.1.2/firebase-firestore.js";
+import { getAuth, createUserWithEmailAndPassword, sendEmailVerification } from 'https://www.gstatic.com/firebasejs/9.1.2/firebase-auth.js';
 
-//const db = getFirestore(app);
+const db = getFirestore(app);
 const Auth = getAuth();
 const signupform = document.getElementById("signup-form");
 
-signupform.addEventListener('submit', (e) =>{
-    e.preventDefault();
+//Sign up values.
+// const saveuser = (name, l_name, email, uid, usertype) =>
+//   addDoc(collection(db, "users"), {
+//     name:name,
+//     l_name:l_name,
+//     email:email,
+//     uid:uid,
+//     usertype:usertype
+//   });
 
-    const signupmail = document.getElementById("signup-email").value;
-    const signuppassword = document.getElementById("signup-password").value;
+
+signupform.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const username = document.getElementById("signup-name").value
+  const userlname = document.getElementById("signup-lname").value
+  const signupmail = document.getElementById("signup-email").value;
+  const signuppassword = document.getElementById("signup-password").value;
+  if (validate_email(signupmail) == false || validate_password(signuppassword) == false) {
+    swal("Oops", "Email o contraseña no válido", "error")
+    return
+  }
+    console.log("Llega");
     createUserWithEmailAndPassword(Auth, signupmail, signuppassword)
-  .then((userCredential) => {
-    // Signed in
-    const user = userCredential.user;
-    signupform.reset();
-    $('#signup').modal('hide')
-    console.log(user);
-    // ...
-  })
-  .catch((error) => {
-    const errorCode = error.code;
-    const errorMessage = error.message;
-    // ..
-  });
+    .then((userCredential) => {
+      // Signed in
+      const user = userCredential.user;
+      user.displayName = username + " " +userlname;
+      console.log(user);
+      signupform.reset();
+      // sendEmailVerification(Auth.currentUser)
+      return setDoc(doc(db, "users", user.uid),{
+        name: username,
+        l_name: userlname,
+        email: signupmail,
+        usertype: 0
+      });
+      // ...
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      // ..
+    });
+    
 })
 
-function validate_email(email){
-  expression = /^[^@]+@\w+(\.\w+)+\w$/
-  if (expression.test(email) == true){
+
+
+function validate_email(email) {
+  const expression = /^[^@]+@\w+(\.\w+)+\w$/
+  if (expression.test(email) == true) {
     //Email structure
     return true
   } else {
@@ -36,10 +65,10 @@ function validate_email(email){
   }
 }
 
-function validate_password(password){
-  if (password < 6){
+function validate_password(password) {
+  if (password < 6) {
     return false
-  } else{
+  } else {
     return true
   }
 }
